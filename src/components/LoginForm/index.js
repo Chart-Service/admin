95d75e069f12fde12d79a756b376881912509d4d
eunjeong.song <hellogine12@gmail.com>
2019-01-16 23:27:54 +0900

    🔨 로그인 화면 수정

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Input = styled.input`
-  width: 100%;
-  font: inherit;
+const Wrapper = styled.section`
+  border-radius: 5px;
+  width: 450px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  color: palevioletred;
+  font-size: 25px;
+  text-align: center;
+  padding: 1em 1em;
   margin: 0;
-  outline: 0;
-  padding: 0;
-  -webkit-transition: background-color 0.3s;
-  transition: background-color 0.3s;
-  border: 1px solid palevioletred;
-  border-radius: 0.25rem;
-  padding: 1rem;
-  border-bottom-left-radius: 0;
-  border-top-left-radius: 0;
 `;
 
 const LoginItem = styled.div`
   color: #ffff;
-  padding: 25px 25px 0;
-  margin: 20px 20px 0;
+  padding: 1em 1em;
   border-radius: 3px;
+  background: #fff;
+  box-shadow: 0 1px 2px 0 rgba(34, 36, 38, 0.15);
+  border-radius: 0.28571429rem;
+  border: 1px solid rgba(34, 36, 38, 0.15);
 `;
 
 const FormField = styled.section`
@@ -36,29 +39,27 @@ const Form = styled.form`
   border-radius: 3px;
 `;
 
+const Input = styled.input`
+  width: 100%;
+  font: inherit;
+  margin: 0;
+  outline: 0;
+  padding: 0;
+  -webkit-transition: background-color 0.3s;
+  transition: background-color 0.3s;
+  padding: 1rem;
+  border: 1px solid palevioletred;
+  border-radius: 0.28571429rem;
+`;
+
 const Button = styled.button`
   width: 100%;
   font-size: 1em;
   background: ${props => (props.primary ? 'palevioletred' : 'white')};
   color: ${props => (props.primary ? 'white' : 'palevioletred')};
   border: 2px solid palevioletred;
-  border-radius: 0.25rem;
+  border-radius: 0.28571429rem;
   padding: 1rem;
-`;
-
-const Title = styled.h1`
-  color: palevioletred;
-  font-size: 20px;
-  text-align: center;
-  padding: 20px 20px 0;
-  margin: 0;
-`;
-
-const Wrapper = styled.section`
-  border-radius: 5px;
-  width: 450px;
-  display: flex;
-  flex-direction: column;
 `;
 
 const LoginForm = () => (
@@ -67,14 +68,14 @@ const LoginForm = () => (
     <LoginItem>
       <Form>
         <FormField>
-          <Input id="login-id" type="text" placeholder="id" />
+          <Input id="login-id" type="text" placeholder="Id" />
         </FormField>
         <FormField>
-          <Input id="login-password" type="password" placeholder="password" />
+          <Input id="login-password" type="password" placeholder="Password" />
         </FormField>
       </Form>
       <Button type="submit" primary>
-        로그인
+        Login
       </Button>
     </LoginItem>
   </Wrapper>
